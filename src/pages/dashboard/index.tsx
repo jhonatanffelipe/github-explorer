30c42701b655c97918a71aa6d39e49a169843327
Jhonatan Nascimento <jhonatanfnm@gmail.com>
2022-10-15 10:58:15 -0300

    Consumindo a API do github

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,72 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
 
-import { Title } from "./styles";
+import api from "../../services/api";
+
+import logoImg from "../../assets/logo.svg";
+
+import { Form, Repositories, Title } from "./styles";
+
+interface IRepository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  const [newRepo, setNewRepo] = useState("");
+
+  const [repositories, setRepositories] = useState<IRepository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<IRepository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+    setNewRepo("");
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        {repositories.map((repository) => (
+          <a href="#" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
